a67de1bcdc421e01fa14818cb579361ffee0416b
Vinícius Batista Barreira <viniciustes@gmail.com>
2018-08-04 19:22:29 -0300

    Commit Initial

---
+++ FirstProjectWithMongoDB/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProjectWithMongoDB.Model
+{
+    class Person
+    {
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+            DateCreated = DateTime.Now;
+        }
+
+        public ObjectId Id { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public bool Active { get; private set; }
+        public DateTime DateCreated { get; private set; }
+    }
+}
---
+++ FirstProjectWithMongoDB/Program.cs
@@ -0,0 +1,68 @@
+﻿using FirstProjectWithMongoDB.Model;
+using MongoDB.Driver;
+using System;
+
+namespace FirstProjectWithMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connect to server
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            //Connect to the database
+            var database = client.GetDatabase("MyFirstScheduleWithMongoDB");
+
+            //collection to persist
+            IMongoCollection<Person> collection = database.GetCollection<Person>("Person");
+
+            //CRUD
+            //InsertPerson(collection);
+            //UpdatePerson(collection);
+            //DeletePerson(collection);
+            ReadPerson(collection);
+
+            Console.ReadKey();
+        }
+
+        private static void ReadPerson(IMongoCollection<Person> collection)
+        {
+            //Filter
+            var filter = Builders<Person>.Filter.Empty;
+
+            var persons = collection.Find(filter);
+
+            persons.ForEachAsync(x =>
+            {
+                Console.WriteLine(string.Concat("Name: ", x.Name, " - Email: ", x.Email));
+            });
+        }
+
+        private static void DeletePerson(IMongoCollection<Person> collection)
+        {
+            //Filter
+            var filter = Builders<Person>.Filter.Where(x => x.Name == "My Name");
+
+            collection.DeleteMany(filter);
+        }
+
+        private static void UpdatePerson(IMongoCollection<Person> collection)
+        {
+            //Filter
+            var filter = Builders<Person>.Filter.Empty;
+
+            //Update
+            var update = Builders<Person>.Update.Set(x => x.Active, true);
+
+            collection.UpdateMany(filter, update);
+        }
+
+        private static void InsertPerson(IMongoCollection<Person> collection)
+        {
+            var person = new Person("My Name", "myemail@mail.com");
+
+            collection.InsertOne(person);
+        }
+    }
+}
